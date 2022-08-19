b601cf43ab9f04a544fad877a586e06c5127ad88
Jhon Santana <jhonsstn@gmail.com>
2022-08-19 20:33:36 -0300

    feat: add match controller factory

--- app/backend/src/factories/match.factory.ts
+++ app/backend/src/factories/match.factory.ts
@@ -1,10 +1,14 @@
 import MatchController from '../controllers/match.controller';
 import SequelizeMatchRepository from '../repositories/match.repository';
+import SequelizeTeamRepository from '../repositories/team.repository';
+import Authenticator from '../services/authentication.service';
 import MatchService from '../services/match.service';
 
 const makeMatch = () => {
   const matchRepo = new SequelizeMatchRepository();
-  const matchService = new MatchService(matchRepo);
+  const authenticator = new Authenticator();
+  const teamRepo = new SequelizeTeamRepository();
+  const matchService = new MatchService(matchRepo, authenticator, teamRepo);
   return new MatchController(matchService);
 };
 
