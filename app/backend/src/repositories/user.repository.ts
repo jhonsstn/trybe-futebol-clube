d68434f7247424e0318ab9edd7079285ab93c871
Jhon Santana <jhonsstn@gmail.com>
2022-08-18 23:00:21 -0300

    refactor: add interface to user repository

--- app/backend/src/repositories/user.repository.ts
+++ app/backend/src/repositories/user.repository.ts
@@ -1,7 +1,8 @@
 import User from '../database/models/user';
+import IUserRepository from './interfaces/user-repository.interface';
 import IUser from './interfaces/user.interface';
 
-class SequelizeUserRepository {
+class SequelizeUserRepository implements IUserRepository {
   findOne = async (email: string): Promise<IUser | null> => {
     const user = await User.findOne({ where: { email } });
     return user;
