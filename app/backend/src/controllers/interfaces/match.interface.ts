fcddf8a9ffbb81a019efa68b3b95e422ff0264fa
Jhon Santana <jhonsstn@gmail.com>
2022-08-19 20:33:26 -0300

    feat: add match controller

--- app/backend/src/controllers/interfaces/match.interface.ts
+++ app/backend/src/controllers/interfaces/match.interface.ts
@@ -1,5 +1,10 @@
 import IMatch from '../../repositories/interfaces/match.interface';
+import IScoreboard from '../../repositories/interfaces/scoreboard.interface';
 
 export default interface IMatchController {
   getAll(): Promise<IMatch[]>;
+  getByProgress(inProgress: string): Promise<IMatch[]>;
+  addNewMatch(match: Omit<IMatch, 'id'>, token: string): Promise<IMatch>
+  finishMatch(id: string): Promise<void>
+  updateMatch(id: string, match: IScoreboard): Promise<void>
 }
--- app/backend/src/controllers/match.controller.ts
+++ app/backend/src/controllers/match.controller.ts
@@ -1,12 +1,31 @@
-import IMatchRepository from '../repositories/interfaces/match-repository.interface';
 import IMatch from '../repositories/interfaces/match.interface';
+import IScoreboard from '../repositories/interfaces/scoreboard.interface';
+import IMatchService from '../services/interfaces/match.interface';
 import IMatchController from './interfaces/match.interface';
 
 class MatchController implements IMatchController {
-  constructor(private matchRepository: IMatchRepository) {}
+  constructor(private matchService: IMatchService) {}
   getAll = async (): Promise<IMatch[]> => {
-    const matches = await this.matchRepository.getAll();
+    const matches = await this.matchService.getAll();
     return matches;
+  };
+
+  getByProgress = async (inProgress: string): Promise<IMatch[]> => {
+    const matches = await this.matchService.getByProgress(inProgress);
+    return matches;
+  };
+
+  addNewMatch = async (match: Omit<IMatch, 'id'>, token: string): Promise<IMatch> => {
+    const newMatch = await this.matchService.addNewMatch(match, token);
+    return newMatch;
+  };
+
+  finishMatch = async (id: string): Promise<void> => {
+    await this.matchService.finishMatch(id);
+  };
+
+  updateMatch = async (id: string, match: IScoreboard): Promise<void> => {
+    await this.matchService.updateMatch(id, match);
   };
 }
 
