c5559191abe5793595a58dfd6b82c78ce4a68e54
Jhon Santana <jhonsstn@gmail.com>
2022-08-19 20:34:38 -0300

    feat: add match controller route adapter

--- app/backend/src/adapters/match.adapter.ts
+++ app/backend/src/adapters/match.adapter.ts
@@ -2,6 +2,22 @@ import { Request, Response } from 'express';
 import MatchController from '../controllers/match.controller';
 
 const adaptMatch = (controller: MatchController) => async (req: Request, res: Response) => {
+  if (req.originalUrl.includes('finish')) {
+    controller.finishMatch(req.params.id);
+    return res.status(200).json({ message: 'Finished' });
+  }
+  if (req.params.id) {
+    controller.updateMatch(req.params.id, req.body);
+    return res.status(200).json({ message: 'Updated' });
+  }
+  if (req.method === 'POST') {
+    const match = await controller.addNewMatch(req.body, req.headers.authorization as string);
+    return res.status(201).json(match);
+  }
+  if (req.query.inProgress !== undefined) {
+    const matches = await controller.getByProgress(req.query.inProgress as string);
+    return res.status(200).send(matches);
+  }
   const matches = await controller.getAll();
   res.status(200).json(matches);
 };
--- app/backend/src/routes/match.routes.ts
+++ app/backend/src/routes/match.routes.ts
@@ -4,6 +4,9 @@ import makeMatch from '../factories/match.factory';
 
 const route = Router();
 
+route.patch('/:id/finish', adaptMatch(makeMatch()));
+route.patch('/:id', adaptMatch(makeMatch()));
 route.get('/', adaptMatch(makeMatch()));
+route.post('/', adaptMatch(makeMatch()));
 
 export default route;
