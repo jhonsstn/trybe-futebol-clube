7e81f05301444552f1669c610fe2dd231a662010
Jhon Santana <jhonsstn@gmail.com>
2022-08-18 21:35:46 -0300

    test: add login route tests

--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -24,7 +24,7 @@ describe('Login route', () => {
   //   (Example.findOne as sinon.SinonStub).restore();
   // })
 
-  it('...', async () => {
+  it('should return status code 200 if user exists', async () => {
     const chaiHttpResponse: Response = await chai
       .request(app)
         .post('/login')
@@ -33,5 +33,52 @@ describe('Login route', () => {
           "password": "secret_admin"
         })
       expect(chaiHttpResponse).to.have.status(200);
+      expect(chaiHttpResponse.body).to.have.property('token');
+  });
+
+  it('should return status code 401 if user does not exist', async () => {
+    const chaiHttpResponse: Response = await chai
+      .request(app)
+        .post('/login')
+        .send({
+          "email": "invalid_main@mail.com",
+          "password": "secret_admin"
+        })
+      expect(chaiHttpResponse).to.have.status(401);
+      expect(chaiHttpResponse.body).to.have.property('message');
+  });
+
+  it('should return status code 401 if password is incorrect', async () => {
+    const chaiHttpResponse: Response = await chai
+      .request(app)
+        .post('/login')
+        .send({
+          "email": "admin@admin.com",
+          "password": "invalid_password"
+        })
+      expect(chaiHttpResponse).to.have.status(401);
+      expect(chaiHttpResponse.body).to.have.property('message');
+  });
+
+  it("should return 400 if email is not provided", async () => {
+    const chaiHttpResponse: Response = await chai
+      .request(app)
+        .post('/login')
+        .send({
+          "password": "secret_admin"
+        })
+      expect(chaiHttpResponse).to.have.status(400);
+      expect(chaiHttpResponse.body).to.have.property('message');
+  });
+
+  it("should return 400 if password is not provided", async () => {
+    const chaiHttpResponse: Response = await chai
+      .request(app)
+        .post('/login')
+        .send({
+          "password": "secret_admin"
+        })
+      expect(chaiHttpResponse).to.have.status(400);
+      expect(chaiHttpResponse.body).to.have.property('message');
   });
 })
