61b02a3d8b62b90cce7d87cfa6d0da8f056eda83
Jhon Santana <jhonsstn@gmail.com>
2022-08-18 22:59:03 -0300

    feat: add not found error

---
+++ app/backend/src/errors/not-found.error.ts
@@ -0,0 +1,9 @@
+class NotFoundError extends Error {
+  statusCode = 404;
+  constructor(message: string) {
+    super(message);
+    this.name = 'NotFoundError';
+  }
+}
+
+export default NotFoundError;
