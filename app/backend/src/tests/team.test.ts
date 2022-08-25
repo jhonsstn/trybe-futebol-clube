f1a3e14c729be6a0b7ddae660121850e63a56926
Jhon Santana <jhonsstn@gmail.com>
2022-08-25 13:54:49 -0300

    feat: add team route tests

--- app/backend/src/tests/team.test.ts
+++ app/backend/src/tests/team.test.ts
@@ -1,11 +1,12 @@
 import * as chai from 'chai';
-// import * as sinon from 'sinon';
+// import * as mocha from 'mocha';
+import * as sinon from 'sinon';
+import { Response } from 'superagent';
+import { app } from '../app';
+import Team from '../database/models/team';
+import { teamList } from './mocks';
 // @ts-ignore
 import chaiHttp = require('chai-http');
-
-import { app } from '../app';
-
-import { Response } from 'superagent';
 
 chai.use(chaiHttp);
 
@@ -13,25 +14,37 @@ const { expect } = chai;
 
 describe('Team route', () => {
 
+  afterEach(()=>{
+    sinon.restore();
+  })
+
   it('should return a list of teams', async () => {
+    sinon.stub(Team, "findAll").resolves(teamList as Team[]);
+
     const chaiHttpResponse: Response = await chai
       .request(app)
         .get('/teams')
 
       expect(chaiHttpResponse).to.have.status(200);
       expect(chaiHttpResponse.body).to.be.an('array');
+      expect(chaiHttpResponse.body).to.be.deep.equal(teamList);
   });
 
   it('should return a team if called with an exiting id', async () => {
+    sinon.stub(Team, "findByPk").resolves(teamList[0] as Team);
+
     const chaiHttpResponse: Response = await chai
       .request(app)
         .get('/teams/1')
 
       expect(chaiHttpResponse).to.have.status(200);
       expect(chaiHttpResponse.body).to.be.an('object');
+      expect(chaiHttpResponse.body).to.be.deep.equal(teamList[0]);
   });
 
   it('should return a 404 if called with an non-existing id', async () => {
+    sinon.stub(Team, "findByPk").resolves(null);
+
     const chaiHttpResponse: Response = await chai
       .request(app)
         .get('/teams/99999999')
